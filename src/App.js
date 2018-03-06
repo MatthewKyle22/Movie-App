4a4345dde8e950cda1267f892cb99b1df951ca5d
Matthew Stevens <mstevens@jrousseau.norman.wdtinc.com>
2018-03-06 15:01:29 -0600

    Added Styled-Components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,67 @@
 import React, { Component } from "react";
-import "./App.css";
-import { getMovies } from "./getMovie/getMovie";
+import { getMovies } from "./getMovie";
 import _ from "lodash";
-import { image } from "superagent/lib/node/parsers";
+// import { image } from "superagent/lib/node/parsers";
+import styled from "styled-components";
+
+// Styling Start
+
+const Wrapper = styled.div`
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  background-color: #f3f3f3;
+  min-height: 100vh;
+  padding-bottom: 5%;
+`;
+
+const MoviesWidget = styled.div`
+  width: 50%;
+  padding: 2%;
+  margin: 0 auto;
+  margin-top: 2%;
+  background-color: white;
+  border-radius: 10px;
+`;
+
+const Searchbar = styled.input`
+  padding: 1em 4em 1em 4em;
+  border-radius: 4px;
+  border-color: lightgray;
+  margin: 1em 2em 1em 2em;
+`;
+
+const Submit = styled.button`
+  padding: 1em;
+  border-radius: 4px;
+  background-color: green;
+  color: white;
+`;
+
+const Delete = styled.button`
+  float: right;
+  padding: 4px;
+  color: white;
+  background-color: #ff3530;
+  border: none;
+  border-radius: 2px;
+`;
+
+const Pic = styled.img``;
+
+// hidden until ready to be used
+// const TryAgain = styled.div`
+//   color: red;
+//   font-weight: bold;
+//   background-color: white;
+//   width: 50%;
+//   margin: 0 auto;
+//   margin-bottom: 2%;
+//   padding: 1%;
+//   border-radius: 10px;
+// `;
+
+// Styling End
 
 class App extends Component {
   constructor() {
@@ -51,40 +110,33 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <div className="App-searchBox">
+      <Wrapper>
+        <div>
           <h1>Movie Search</h1>
           {/* {!this.state.isHidden && <InvalidMovie />} */}
-          <input
-            className="App-input"
+          <Searchbar
             type="text"
             placeholder="Enter A Movie Name"
             name="a"
             id="a"
           />
-          <button
-            className="App-button"
+          <Submit
             onClick={() => this.runSearch(document.getElementById("a").value)}
           >
             Search
-          </button>
+          </Submit>
         </div>
         {_.orderBy(this.state.movies, "title", "asc").map(movie => {
           return (
-            <div className="App-movies" key={movie.title}>
-              <img src={movie.poster} width="150" height="250" />
+            <MoviesWidget key={movie.title}>
+              <Pic src={movie.poster} width="150" height="250" />
               <h2>{movie.title}</h2>
               <p>{movie.description} </p>
-              <button
-                className="App-delete"
-                onClick={() => this.removeMovie(movie)}
-              >
-                Delete
-              </button>
-            </div>
+              <Delete onClick={() => this.removeMovie(movie)}>Delete</Delete>
+            </MoviesWidget>
           );
         })}
-      </div>
+      </Wrapper>
     );
   }
 }
@@ -92,12 +144,12 @@ class App extends Component {
 /* Add this when movie is not found */
 
 // const InvalidMovie = () => (
-//   <div className="App-error">
+//   <TryAgain>
 //     <p>
 //       The Movie Title You Have Entered Is Not Valid.
 //       <br /> Please Enter A Valid Movie Title
 //     </p>
-//   </div>
+//   </TryAgain>
 // );
 
 export default App;
